b7e67762083cada4eebb301ff2f196723ed08fec
Firmino Neto <firminoneto11@hotmail.com>
2021-06-17 22:46:04 -0300

    Made some minor changes about the css display of the <main> tags and some sql files

--- core/static/js/quantity.js
+++ core/static/js/quantity.js
@@ -1,31 +1,23 @@
-
+/*
+This function will fill the first <td> tag of each row with counter that is incremented
+for the amount of rows. Also, will change the information of how many elements are in 
+the table, bellow the table.
+*/
 function get_quantity(event) {
+    // Selecting the elements and couting the occurences
     const tbody = document.getElementById('tbody')
-    const children = tbody.children
+    const tbody_children = tbody.children
     let quantity = 0
-    for (let child of children) {
-        if (child.className !== 'tbody_child no-display') {
-            quantity += 1
-        }
-    }
-    const quantity_display = document.getElementById('quantity_display')
-    quantity_display.innerHTML = `${quantity} items`
-}
-
-function task_amount(event) {
-    const tbody = document.getElementById('tbody')
-    const children = tbody.children
-    let quantity = 0
-    for (let tr of children) {
+    for (let tr of tbody_children) {
         if (tr.className !== 'tbody_child no-display') {
             quantity += 1
             tr.firstElementChild.innerHTML = `${quantity}`
         }
     }
+    // Updating the information of elements amount
+    document.getElementById('quantity_display').innerHTML = `${quantity} items`
 }
 
+// Binding the event listeners to the function above
 document.addEventListener('DOMContentLoaded', get_quantity)
 document.getElementById('radio_elements').addEventListener('click', get_quantity)
-
-document.addEventListener('DOMContentLoaded', task_amount)
-document.getElementById('radio_elements').addEventListener('click', task_amount)
