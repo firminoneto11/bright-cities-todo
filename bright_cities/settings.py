8c25c0fe6eafe884913d460312e6381745b5ded9
Firmino Neto <firminoneto11@hotmail.com>
2021-06-19 13:11:37 -0300

    psycopg binaries

--- bright_cities/settings.py
+++ bright_cities/settings.py
@@ -80,10 +80,24 @@ WSGI_APPLICATION = 'bright_cities.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 
+DATABASES = {
+    'default': {
+        'ENGINE': config('ENGINE'),
+        'HOST': config('HOST'),
+        'PORT': config('PORT'),
+        'NAME': config('NAME'),
+        'USER': config('USER'),
+        'PASSWORD': config('PASSWORD')
+    }
+}
+
+
 # Heroku Database
+"""
 DATABASES = {
     'default': dj_database_url.config()
 }
+"""
 
 
 # Password validation
